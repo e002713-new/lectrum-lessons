fb2694ec9e37dbc83229c6bca1997ed60ba41d6f
Dima Panasevich <e002713@gmail.com>
2020-02-12 16:47:08 +0300

    add some lesson-10

---
+++ lesson-10/2.js
@@ -0,0 +1,61 @@
+/**
+ * Задача 2.
+ *
+ * Напишите функцию calculate(), которая в качестве аргументов принимает неограниченное количество функций.
+ *
+ * При запуске calculate() последовательно запускает коллбек-функции из аргументов.
+ * Каждая коллбек-функция из цепочки в качестве своего аргумента принимает то, что возвращает предыдущая коллбек-функция.
+ * То есть возвращаемое значение каждой коллбек-функции из цепочки
+ * становится доступным из параметра следующей коллбек-функции в цепочке.
+ *
+ * Первая коллбек-функция не принимает параметров.
+ *
+ * Функция calculate() должна вернуть результат выполнения последней коллбек-функции из цепочки.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов функции calculate() не является функцией;
+ * - Любая функция из аргументов не вернула значение.
+ */
+
+// Решение
+
+function validate(param, argCount, firstParam = true) {
+    if (typeof param !== 'function' && firstParam) {
+        throw new Error(`${argCount} is not a function`);
+    } else if (param === undefined && !firstParam) {
+        throw new Error(`${argCount} function don't return result`)
+    }
+
+
+}
+
+function calculate(...rest) {
+    let result = null;
+
+    for (let [key, item] of rest.entries()) {
+        validate(item, key + 1);
+
+        result = item(result);
+
+        validate(result, key + 1, false);
+    }
+
+    return result;
+}
+
+
+const result = calculate(
+    () => {
+        return 7;
+    },
+    prevResult => {
+        return prevResult + 4;
+    },
+    prevResult => {
+        return prevResult * 5;
+    },
+);
+
+console.log(result); // 55
+
+exports.calculate = calculate;
